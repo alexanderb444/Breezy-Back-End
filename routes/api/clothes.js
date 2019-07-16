1574dec80919ceb6b6b9aa20ff3950feb3ce3e7f
alexanderb444 <bishop.alexander@gmail.com>
2019-07-16 11:21:08 +1000

    fixed validator

--- routes/api/clothes.js
+++ routes/api/clothes.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const config = require('config')
-const {check, validationResult} = require('express-validator/check')
+const {check, validationResult} = require('express-validator')
 
 const Clothes = require('../../models/Clothes')
 
